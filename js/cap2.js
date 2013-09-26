8b57f930a4ca839dbe5ab4a94d7f782174f7c385
Yanina Aular <yanina.aular@vauxoo.com>
2013-09-26 10:56:12 -0430

    [IMP] files reestructuring

---
+++ js/cap2.js
@@ -0,0 +1,19 @@
+/********************************** Accesando a Elmentos HTML CAPITULO 2 *************************/
+
+//El signo $(.. tambien puede llamarse con
+//jQuery(.. significa que se está interactuando
+//con jQuery.
+
+//La funcion $document.ready es llamada cuando la pagina ha finalizado de cargar
+
+$(document).ready( function(){
+
+        //alert("Hello World!");
+        //Simple alerta Hello World!    
+
+        alert( $('img').attr('alt') ); 
+        alert(jQuery('img').attr('height'));
+        //Tomara la primera imagen 
+        //del documento y consulta el atributo dentro de attr('atributo_a_consultar')
+       
+});
--- js/myscript.js
+++ js/myscript.js
@@ -1,3 +1,4 @@
+/************************************************** CAPITULO 1 ***********************************/
 
 //El signo $(.. tambien puede llamarse con
 //jQuery(.. significa que se está interactuando
@@ -8,11 +9,13 @@
 $(document).ready( function(){
 
         //alert("Hello World!");
-        
+        //Simple alerta Hello World!    
+
         alert( $('img').attr('alt') ); 
+        alert(jQuery('img').attr('height'));
         //Tomara la primera imagen 
-        //del documento y consulta el atributo alt
-        
+        //del documento y consulta el atributo dentro de attr('atributo_a_consultar')
+       
 });
 
 
