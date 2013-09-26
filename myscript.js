5206ce3bf536271cd27ceb9918a05a1685865d76
Yanina Aular <yanina.aular@vauxoo.com>
2013-09-26 10:41:21 -0430

    [ADD] new imagen and $(img) that obtain first image in the page

--- myscript.js
+++ myscript.js
@@ -1,1 +1,21 @@
-alert("Hello World!");
+
+//El signo $(.. tambien puede llamarse con
+//jQuery(.. significa que se está interactuando
+//con jQuery.
+
+//La funcion $document.ready es llamada cuando la pagina ha finalizado de cargar
+
+$(document).ready( function(){
+
+        //alert("Hello World!");
+        
+        alert( $('img').attr('alt') ); 
+        //Tomara la primera imagen 
+        //del documento y consulta el atributo alt
+        
+});
+
+
+
+
+
